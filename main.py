f15b871e6f0bc79e04a22c053b018a326330381d
Abhijit Darekar <darekarabhijit63@gmail.com>
2022-09-19 21:22:45 +0530

    Get request to precit if message is HAM or SPAM

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-from tkinter.messagebox import RETRY
+import joblib
+import re
+import warnings
+warnings.filterwarnings('ignore')
 from fastapi import FastAPI
 
-app=FastAPI()
+app = FastAPI()
 
-@app.get("/")
-async def get_root():
-    return {"Hello User":" Welcome to Spam Detection App"}
+model = joblib.load('Final_model.joblib')
+
+def preprocessor(text):
+    text = re.sub('<[^>]*>', '', text) # Effectively removes HTML markup tags
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
+    text = re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', '') 
+    return text
+ 
+def classify_message(model, message):
+    message = preprocessor(message)
+    label = model.predict([message])[0]
+    return {'label': label}
+
+@app.get('/')
+def get_root():
+    return {'Hello': 'Welcome to the spam detection APP'}
+
+ 
+@app.get('/spam_detection_path/{message}')
+async def detect_spam_path(message: str):
+	return classify_message(model, message)
